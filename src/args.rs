ef63e31c36244e653e39648782e701089541fdfa
Juliette Cordor <grassojames5@gmail.com>
2022-05-03 16:35:54 +1000

    fixes

--- src/args.rs
+++ src/args.rs
@@ -1,14 +1,64 @@
+use std::{error::Error, fmt::Display};
+
 use argh::FromArgs;
+
+pub enum Measurement {
+    Byte,
+    Kilobyte,
+    Megabyte,
+    Gigabyte,
+}
+
+#[derive(Debug)]
+pub enum ArgError {
+    InvalidMeasurement(String),
+}
+
+impl Error for ArgError {}
+
+impl Display for ArgError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ArgError::InvalidMeasurement(measurement) => write!(
+                f,
+                "Invalid Measurement: {}. Must be kb, mb, or gb",
+                measurement,
+            ),
+        }
+    }
+}
+
+impl From<Measurement> for u64 {
+    fn from(measurement: Measurement) -> u64 {
+        let i = measurement as u8;
+
+        1024_i32.pow(i as u32) as u64
+    }
+}
+
+impl Measurement {
+    pub fn into_bytes(self) -> Vec<u8> {
+        let size: u64 = self.into();
+
+        vec![0u8; size as usize]
+    }
+}
 
 #[derive(FromArgs)]
 #[argh(description = "A simple demo file generator")]
-pub struct Args {
+pub struct GenArgs {
     #[argh(positional)]
-    size: usize,
+    pub size: usize,
 
     #[argh(option, short = 'm', description = "the data measurement to write")]
-    measurement: Option<char>,
+    pub measurement: Option<String>,
 
-    #[argh(option, short = 'f', description = "the file to write to")]
-    filename: Option<String>,
+    #[argh(option, short = 'p', description = "the file to write to")]
+    pub path: Option<String>,
+}
+
+impl GenArgs {
+    pub fn parse() -> Self {
+        argh::from_env()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,68 @@
 mod args;
 
-macro_rules! square {
-    ($num:expr) => {
-        $num * $num
-    };
+use std::{fs::File, io::Write};
+
+use args::{ArgError, GenArgs, Measurement};
+
+fn main() -> anyhow::Result<()> {
+    let args = GenArgs::parse();
+
+    let measurement: u64 = if let Some(measurement) = args.measurement.map(|x| x.to_lowercase()) {
+        match measurement.as_str() {
+            "byte" | "b" => Ok(Measurement::Byte),
+            "kilobyte" | "kb" | "k" => Ok(Measurement::Kilobyte),
+            "megabyte" | "mb" | "m" => Ok(Measurement::Megabyte),
+            "gigabyte" | "gb" | "g" => Ok(Measurement::Gigabyte),
+            _ => Err(ArgError::InvalidMeasurement(measurement.to_string())),
+        }
+    } else {
+        Ok(Measurement::Byte)
+    }?
+    .into();
+
+    let mut size: u64 = args.size as u64 * measurement;
+
+    let cwd = std::env::current_dir()?;
+    let path = cwd.join(args.path.unwrap_or_else(|| "file.txt".into()));
+
+    let mut file = File::create(path)?;
+
+    if size > u64::from(Measurement::Gigabyte) {
+        let gigs: u64 = size / u64::from(Measurement::Gigabyte);
+        size -= gigs * u64::from(Measurement::Gigabyte);
+
+        let bytes = Measurement::Gigabyte.into_bytes();
+        for _ in 0..gigs {
+            file.write_all(&bytes)?;
+        }
+    }
+
+    if size > u64::from(Measurement::Megabyte) {
+        let megas: u64 = size / u64::from(Measurement::Megabyte);
+        size -= megas * u64::from(Measurement::Megabyte);
+
+        let bytes = Measurement::Megabyte.into_bytes();
+        for _ in 0..megas {
+            file.write_all(&bytes)?;
+        }
+    }
+
+    if size > u64::from(Measurement::Kilobyte) {
+        let kilos: u64 = size / u64::from(Measurement::Kilobyte);
+        size -= kilos * u64::from(Measurement::Kilobyte);
+
+        let bytes = Measurement::Kilobyte.into_bytes();
+        for _ in 0..kilos {
+            file.write_all(&bytes)?;
+        }
+    }
+
+    if size > 0 {
+        let bytes = Measurement::Byte.into_bytes();
+        for _ in 0..size {
+            file.write_all(&bytes)?;
+        }
+    }
+
+    Ok(())
 }
-
-const BYTE: u64 = 0x01;
-const KILOBYTE: u64 = 1024;
-const MEGABYTE: u64 = square!(KILOBYTE);
-
-fn main() {}
