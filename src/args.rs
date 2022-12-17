69ce5dc4f72378059021fc76fb987e7f2ca46b8d
Juliette Cordor <grassojames5@gmail.com>
2022-12-17 16:34:39 +1100

    fix

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,11 @@
 use std::str::FromStr;
 
 use clap::Parser;
+use derive_more::Display;
 
 use crate::error::ArgError;
 
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Display, Default, Clone, Copy)]
 pub enum Measurement {
     #[default]
     Byte,
